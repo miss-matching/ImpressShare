315f8c0ecaa38936e05e0f1b09b03ce88d487c99
rocky <tak.koroki@gmail.com>
2013-08-04 14:24:26 +0900

    たいぽ

--- script/verify_env.rb
+++ script/verify_env.rb
@@ -2,4 +2,4 @@ command = 'script/markdown2impress/bin/markdown2impress.pl'
 target = 'script/markdown2impress/README.md'
 
 result_for_markdown2impress = system( "#{command} #{target}" )
-p 'you are redy to go widh markdown2impress!' if result_for_markdown2impress
+p 'you are ready to go widh markdown2impress!' if result_for_markdown2impress
