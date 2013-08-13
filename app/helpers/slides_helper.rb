bdd4a2bdac32a9061c9356ebe4ba6ff79abd5bee
rocky <tak.koroki@gmail.com>
2013-08-13 21:42:17 +0900

    twitterのprofile imageをオリジナルサイズにしましたよん

--- app/helpers/slides_helper.rb
+++ app/helpers/slides_helper.rb
@@ -1,6 +1,12 @@
 #coding: utf-8
 module SlidesHelper
 
+  def convert_profile_url_to_the_origin_size( normal_size_image_url )
+    return '' if normal_size_image_url.blank?
+    normal_size_image_url.sub( "_normal","" )
+
+  end
+
   def kind_options
     [ [ 'Github / Gist', Slide::KIND_GITHUB ],
       [ 'Markdown',Slide::KIND_MARKDOWN]]
